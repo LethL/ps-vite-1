2ebc293a33c7b26834dd1857c9c32101f107ea9a
dgoncharov <dgoncharov@neoflex.ru>
2025-12-22 19:06:54 +0700

    3-events

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup>
     import Button from './components/Button.vue';
+    import Score from './components/Score.vue';
+    import Card from './components/Card.vue';
+
+    function handleTurnOverCard(event) {
+        console.log(event);
+    }
 </script>
 
 <template>
+    <header class="header">
+        ЗАПОМНИ СЛОВО
+        <Score value="100" /> 
+    </header>
+    <Card @turn-over="handleTurnOverCard" />
     <Button />
 </template>
 
-<style scoped></style>
+<style scoped>
+    .header {
+        padding: 32px 62px;
+        font-weight: 700;
+        color: #222222;
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,52 @@
+<script setup>
+    const emit = defineEmits(['turn-over', 'change-status']);
+
+    function handleTurnOverCard() {
+        emit('turn-over', 'Card turned over');
+    }
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="inner">
+            <p class="text">01</p>
+            <p class="text bottom" @click="handleTurnOverCard()">ПЕРЕВЕНУТЬ</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .wrapper {
+        background-color: #FFFFFF;
+        box-shadow: 0px 0px 16px 0px #0000001A;
+        border-radius: 16px;
+        padding: 28px 19px;
+        width: 250px;
+        height: 376px;
+    }
+
+    .wrapper:hover {
+        box-shadow: 0px 0px 16px 0px #00000033;
+    }
+
+    .inner {
+        border: 1px solid #CCE8FF;
+        height: 100%;
+        border-radius: 12px;
+        position: relative;
+    }
+
+    .text {
+        position: absolute;
+        top: -10px;
+        left: 16px;
+        margin: 0;
+        background-color: #FFFFFF;
+    }
+
+    .bottom {
+        top: 98%;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+</style>
